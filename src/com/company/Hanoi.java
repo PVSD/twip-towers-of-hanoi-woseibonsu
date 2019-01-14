b3fef6ccf4bd34567cbd386a73b328295e5091e3
wo580 <winstonobonsu@gmail.com>
2019-01-14 11:22:15 -0500

    1.1

---
+++ src/com/company/Hanoi.java
@@ -0,0 +1,19 @@
+package com.company;
+
+/**
+ * Created by wo580 on 1/14/19.
+ */
+public class Hanoi {
+
+    private int discs = 0;
+
+    public Hanoi(int discs)
+    {
+        this.discs = discs;
+    }
+
+
+
+    public int getDiscs() { return discs; }
+
+}
